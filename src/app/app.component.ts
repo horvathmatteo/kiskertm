6f75237f5264770394c80bf4d5f1753272a99b17
Horváth Máté <h764070@inf.u-szeged.hu>
2021-03-29 21:03:45 +0200

    google analytics fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
+import { getHtmlTagDefinition } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'kiskertm';
+
+  constructor(private router: Router){
+    this.router.events.subscribe(event => {
+      if( event instanceof NavigationEnd) {
+        gtag('config', 'G-B5EJ7FF1G8', {
+          'page_path': event.urlAfterRedirects
+        })
+      }
+    });
+  }
 }
