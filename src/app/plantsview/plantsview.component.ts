f06f2e43fe820dad489622bb9e27104786dd64cd
Horváth Máté <h764070@inf.u-szeged.hu>
2021-03-29 20:30:44 +0200

    plants view implement

--- src/app/plantsview/plantsview.component.ts
+++ src/app/plantsview/plantsview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Plant } from '../models/plant.model';
@@ -9,19 +9,27 @@ import { KiskertmService } from '../services/kiskertm.service';
   templateUrl: './plantsview.component.html',
   styleUrls: ['./plantsview.component.scss']
 })
-export class PlantsviewComponent implements OnInit {
+export class PlantsviewComponent implements OnInit, OnChanges {
 
   plants?: Plant[];
   category?: any;
+  categoryPlants?: Plant[];
 
-  constructor(private plantService: KiskertmService, private route: ActivatedRoute) { }
+  constructor(private plantService: KiskertmService, private route: ActivatedRoute) {
+   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
 
   ngOnInit(): void {
+    this.categoryPlants = [];
+    
     this.route.queryParams.subscribe(params => {
+      this.categoryPlants = [];
       this.category = params.category;
-      console.log(this.category);
+      this.retrievePlants();
     });
-    this.retrievePlants();
   }
 
   retrievePlants(): void {
@@ -33,6 +41,23 @@ export class PlantsviewComponent implements OnInit {
         )
     ).subscribe(data => {
       this.plants = data;
+      if(this.category === "összes") {
+        this.categoryPlants = this.plants;
+      }else {
+        this.plants?.forEach(element => {
+          if(element.category == this.category) {
+          this.categoryPlants?.push(element);
+          }
+        });
+      }
+    });
+  }
+
+  filterByCategory(category: string): void {
+    this.plants?.forEach(element => {
+      if(element.category == "összes") {
+        this.categoryPlants?.push(element);
+      } 
     });
   }
 
